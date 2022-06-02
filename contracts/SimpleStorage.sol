7429e3c1a32506e24ce4bf89b56e49fb3710092e
imskanand <the.anand3099@gmail.com>
2022-06-02 23:37:20 +0530

    function in Solidity -> Pure , view

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,7 @@
-/*
- *  Solidity contains the License of the contract
- *   The version which is needed to  be  used must be specified by using the operators.
- */
+    /*
+        Solidity contains the License of the contract
+        The version which is needed to  be  used must be specified by using the operators.
+    */
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7; //  Using the version greater than that version
@@ -9,14 +9,31 @@ pragma solidity ^0.8.7; //  Using the version greater than that version
 // The contract in solidity start from here only.
 contract SimpleStorage {
     /*
-    Types in  Solidity
-    boolean, unit, int, address, bytes
-    Every type get their value by default
+        Types in  Solidity
+        boolean, unit, int, address, bytes
+        Lower is unit8 -> uint256 and step sizeof 8 UP similarly for int8 -> uint256
+        Every type get their value by default
+        Default value of uint is  = 0
+        Bytes is converted to  hexadecimal like 0x and max size is byte32
    */
-    bool isFavoriteNumber;
-    unit256 favoriteNumber = 5; // Lower is unit8 and step sizeof 8 UP lly for int8
-    string myName = "imskanand";
-    int256 favoriteInt = -5;
-    address myAddress = 0x062B90898540ca8B842C2f317EfDB949316387fe;
-    bytes32 favoriteByte = "cat"; //  converted to  hexadecimal like 0x and max size is byte32
+
+    uint256 favoriteNumber = 5;
+
+    /*
+        Only view and Pure function doesn't  costs gas.
+        View function doesn't allows the modification of the state
+        when the view function run inside the store function it will cost gas
+        Pure function doesn't allows reading as well as changing of any data in the blockchain
+    */
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        returns favoriteNumber;
+    }
+
+    function add() public pure returns (uint256) {
+        returns (1+6);
+    }
 }
