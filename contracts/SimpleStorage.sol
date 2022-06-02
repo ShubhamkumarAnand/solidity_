02a42e63d7fa0c83dff73996f2b5896088970e3f
imskanand <the.anand3099@gmail.com>
2022-06-02 12:59:38 +0530

    Added the comment to the file

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,2 +1,6 @@
+/*
+ *  Solidity contains the License of the contract
+ *   The version which is needed to  be  used must be specified by using the operators.
+ */
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.7;
+pragma solidity ^0.8.7; //  Using the version greater than that version
