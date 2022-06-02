6c914198a7e0c25509b4006da8566b0bb42c0236
imskanand <the.anand3099@gmail.com>
2022-06-02 19:20:50 +0530

    First Solidity Contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,5 +2,8 @@
  *  Solidity contains the License of the contract
  *   The version which is needed to  be  used must be specified by using the operators.
  */
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7; //  Using the version greater than that version
+
+contract SimpleStorage {}
