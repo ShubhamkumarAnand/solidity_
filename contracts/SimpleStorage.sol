af5f7abe1dfff6f432325ee24c7bfeac3a5314e6
imskanand <the.anand3099@gmail.com>
2022-06-02 19:51:07 +0530

    Types in solidity

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -6,4 +6,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7; //  Using the version greater than that version
 
-contract SimpleStorage {}
+// The contract in solidity start from here only.
+contract SimpleStorage {
+    /*
+    Types in  Solidity
+    boolean, unit, int, address, bytes
+    Every type get their value by default
+   */
+    bool isFavoriteNumber;
+    unit256 favoriteNumber = 5; // Lower is unit8 and step sizeof 8 UP lly for int8
+    string myName = "imskanand";
+    int256 favoriteInt = -5;
+    address myAddress = 0x062B90898540ca8B842C2f317EfDB949316387fe;
+    bytes32 favoriteByte = "cat"; //  converted to  hexadecimal like 0x and max size is byte32
+}
