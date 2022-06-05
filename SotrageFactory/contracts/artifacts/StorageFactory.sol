74550d06ef6d7933fff6e35d34412dce09506223
imskanand <the.anand3099@gmail.com>
2022-06-05 23:47:46 +0530

    Correction

--- SotrageFactory/contracts/artifacts/StorageFactory.sol
+++ SotrageFactory/contracts/artifacts/StorageFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0; // This is saying anything in the range of 0.8 is ok
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorageArray;
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
