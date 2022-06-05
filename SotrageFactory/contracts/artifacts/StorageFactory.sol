57fd063da4d230c5be66c57205353b05132a5ec2
imskanand <the.anand3099@gmail.com>
2022-06-05 23:44:59 +0530

    Adding import and SimpleStorage Array creation

---
+++ SotrageFactory/contracts/artifacts/StorageFactory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0; // This is saying anything in the range of 0.8 is ok
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+}
